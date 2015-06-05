e3504b23236462efc9543ec05013894c057de8e5
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-05 15:59:37 +0300

    build becomes style, compile becomes build

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,2 @@
 gulp = require('beverage')(require('gulp'));
-gulp.task('dev', 'DEVELOP', ['serve', 'sourcegate:watch', 'build:watch', 'bsync']);
+gulp.task('dev', 'DEVELOP', ['serve', 'sourcegate:watch', 'style:watch', 'bsync']);
